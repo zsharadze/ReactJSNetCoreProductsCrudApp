e1063e4a170a55d316cd51fa43ff90c52ae4e42c
Zviad Sharadze <zsharadze9@gmail.com>
2025-02-22 19:49:59 +0400

    refactored code

--- ASPNetCoreAPI/ASPNetCoreAPI/Controllers/ProductController.cs
+++ ASPNetCoreAPI/ASPNetCoreAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using ASPNetCoreAPI.Data;
 using ASPNetCoreAPI.Extensions;
+using ASPNetCoreAPI.Infrastructure;
 using ASPNetCoreAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,74 +14,89 @@ namespace ASPNetCoreAPI.Controllers
     {
         private readonly ILogger<ProductController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly ImageSaver _imageSaver;
         public ProductController(ILogger<ProductController> logger,
-            ApplicationDbContext context)
+            ApplicationDbContext context, ImageSaver imageSaver)
         {
             _logger = logger;
             _context = context;
+            _imageSaver = imageSaver;
         }
 
         [HttpGet]
-        public async Task<ApiResponseProducts> GetProducts(int? pageSize, int? pageIndex)
+        [ProducesResponseType(typeof(ApiResponseProducts), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetProducts(int? pageSize, int? pageIndex)
         {
             var result = new ApiResponseProducts();
             PagerHelper pagerHelper = new PagerHelper(_context.Products.Count(), pageIndex.Value, pageSize.Value, "");
             result.Pager = pagerHelper.GetPager;
             result.ProductList = await _context.Products.Skip((pagerHelper.CurrentPage - 1) * pagerHelper.PageSize).Take(pagerHelper.PageSize).ToListAsync();
             result.Success = true;
-            return result;
+            return Ok(result);
         }
 
         [HttpGet]
-        public async Task<Product> GetProduct(int id)
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
             product.Views++;
-            await _context.SaveChangesAsync();
-            return product;
-        }
-
-        [HttpPost]
-        public async Task<ApiResponse> AddProduct([FromForm] Product product)
-        {
-            if (string.IsNullOrEmpty(product.Title) || string.IsNullOrEmpty(product.Description) || product.ImageFile == null || product.ImageFile.Length == 0)
-                return new ApiResponse() { Success = false, Message = "Fill necessary fields" };
-
             try
             {
-                product.ImgName = await SaveImage(product.ImageFile);
-                await _context.Products.AddAsync(product);
                 await _context.SaveChangesAsync();
-                return new ApiResponse() { Success = true, Message = "Product successfully added" };
             }
             catch (Exception ex)
             {
-                return new ApiResponse() { Success = false, Message = "Error when saving product. " + ex.Message };
+                return Ok(new ApiResponse() { Success = false, Message = "Error saving views count for product. " + ex.Message });
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> AddProduct([FromForm] Product product)
+        {
+            if (string.IsNullOrEmpty(product.Title) || string.IsNullOrEmpty(product.Description) || product.ImageFile == null || product.ImageFile.Length == 0)
+                return Ok(new ApiResponse() { Success = false, Message = "Fill necessary fields" });
+
+            try
+            {
+                product.ImgName = await _imageSaver.SaveImage(product.ImageFile);
+                await _context.Products.AddAsync(product);
+                await _context.SaveChangesAsync();
+                return Ok(new ApiResponse() { Success = true, Message = "Product successfully added" });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ApiResponse() { Success = false, Message = "Error when saving product. " + ex.Message });
             }
         }
 
         [HttpPut]
-        public async Task<ApiResponse> UpdateProduct([FromForm] Product product)
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateProduct([FromForm] Product product)
         {
             if (product.ImageFile != null)
             {
                 System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\imgs", product.ImgName));
-                product.ImgName = await SaveImage(product.ImageFile);
+                product.ImgName = await _imageSaver.SaveImage(product.ImageFile);
             }
             _context.Products.Update(product);
             try
             {
                 await _context.SaveChangesAsync();
-                return new ApiResponse() { Success = true, Message = "Product successfully updated" };
+                return Ok(new ApiResponse() { Success = true, Message = "Product successfully updated" });
             }
             catch (Exception ex)
             {
-                return new ApiResponse() { Success = false, Message = "Error when updating product. " + ex.Message };
+                return Ok(new ApiResponse() { Success = false, Message = "Error when updating product. " + ex.Message });
             }
         }
 
         [HttpDelete]
-        public async Task<ApiResponse> DeleteProduct(int id)
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteProduct(int id)
         {
             try
             {
@@ -89,25 +105,12 @@ namespace ASPNetCoreAPI.Controllers
                 await _context.SaveChangesAsync();
                 if (!string.IsNullOrEmpty(productToDelete.ImgName))
                     System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\imgs", productToDelete.ImgName));
-                return new ApiResponse() { Success = true, Message = "Product successfully deleted" };
+                return Ok(new ApiResponse() { Success = true, Message = "Product successfully deleted" });
             }
             catch (Exception ex)
             {
-                return new ApiResponse() { Success = false, Message = "Error when deleting product. " + ex.Message };
+                return Ok(new ApiResponse() { Success = false, Message = "Error when deleting product. " + ex.Message });
             }
-        }
-
-        private async Task<string> SaveImage(IFormFile file)
-        {
-            var fileExt = Path.GetFileName(Path.GetExtension(file.FileName));
-            var fileName = Guid.NewGuid().ToString() + fileExt; ;
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\imgs", fileName);
-            using (var fileStream = new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(fileStream);
-            }
-
-            return fileName;
         }
     }
 }
---
+++ ASPNetCoreAPI/ASPNetCoreAPI/Infrastructure/ImageSaver.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPNetCoreAPI.Infrastructure
+{
+    public class ImageSaver
+    {
+        public async Task<string> SaveImage(IFormFile file)
+        {
+            var fileExt = Path.GetFileName(Path.GetExtension(file.FileName));
+            var fileName = Guid.NewGuid().ToString() + fileExt; ;
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\imgs", fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return fileName;
+        }
+    }
+}
